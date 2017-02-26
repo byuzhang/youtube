97ebe04e80d3e60be08c6b8425829de69d24333d
ifishy21 <619470299@qq.com>
2017-02-26 22:23:03 +0800

    update config

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 ### 简介:
 
-利用React框架完成的简化版YouTube视频搜索应用。
+利用React库完成的简化版YouTube视频搜索应用。
 
-[Live Demo Here](https://byuzhang.github.io/youtube/)
+[Live Demo Here](https://icyfish.github.io/youtube/)
 **(需自备梯子)**
 
 ### 主要工具:
@@ -17,9 +17,9 @@
 
 ### 在本地查看:
 
-1. `git clone git@github.com:byuzhang/youtube.git`
+1. `git clone git@github.com:icyfish/youtube.git`
 2. `cd youtube`
 3. `npm install`
 4. `npm start`
 
-也可点击[这里](https://byuzhang.github.io/youtube/)直接查看。
+也可点击[这里](https://icyfish.github.io/youtube/)直接查看。
--- src/components/Address.js
+++ src/components/Address.js
@@ -5,10 +5,10 @@ const Address = (props) => {
   return (
     <Row bsClass="row footer">
       <Col md={5}>
-        Created by <a href="https://byuzhang.github.io/me/" target="_blank">Bingyu Zhang</a>
+        Created by <a href="https://icyfish.github.io/" target="_blank">Bingyu Zhang</a>
       </Col>
       <Col md={7} mdPush={5}>
-        Powered by <a href="https://github.com/byuzhang/youtube/"target="_blank">Simplified YouTube</a>
+        Powered by <a href="https://github.com/icyfish/youtube/"target="_blank">Simplified YouTube</a>
       </Col>
     </Row>
   )
